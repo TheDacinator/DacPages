0af7e3a97c5ca5810e8358aa0fd7d90b88fd6f52
TheDacinator <dacman9@gmail.com>
2016-11-04 18:01:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var shadow = function(x) {
+	x.style["box-shadow"] = "0 0 25px black";
+}
+var noshadow = function(x) {
+	x.style["box-shadow"] = "none";
+}
+function resize() {
+	if (window.innerWidth < 1120) {
+		document.getElementById("boxes").style["max-width"] = 548;
+	} else {
+		document.getElementById("boxes").style["max-width"] = 1106;
+	}
+	console.log('hi');
+}
