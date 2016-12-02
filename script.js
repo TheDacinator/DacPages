a038a8b812928e91cbe818a7ae9b4b9f0f3d76a7
TheDacinator <dacman9@gmail.com>
2016-12-02 10:39:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-var shadow = function(x) {
-	x.style["box-shadow"] = "0 0 25px black";
-}
-var noshadow = function(x) {
-	x.style["box-shadow"] = "none";
-}
 function resize() {
 	if (window.innerWidth < 1120) {
 		document.getElementById("boxes").style["max-width"] = 548;
